b30aae5c2461751ea5307ec2758e29290f5732ad
seileremerson <seiler.emerson@gmail.com>
2022-02-03 21:22:45 -0300

    Listagem de funcionalidaes
    Adicionado a listagem dos itens dentro do array

--- script.js
+++ script.js
@@ -1,116 +1,331 @@
-//VÁRIAVEIS RESPONSÁVEIS PELAS TELAS
+
+// ============================== VÁRIAVEIS RESPONSÁVEIS PELAS TELAS ==============================
 const GetDisplay = {
     displayDashboard: document.querySelector('#dashboard'),
     displayClient: document.querySelector('#client'),
     displayClientRegister: document.querySelector('#client-register'),
-    displayAnnouncement: document.querySelector('#announcement'),
-    displayAnnouncementRegister: document.querySelector('#announcement-register'),
-    displayAnnouncementView: document.querySelector('#announcement-view'),
+    displayAds: document.querySelector('#ads'),
+    displayAdsRegister: document.querySelector('#ads-register'),
+    displayAdsView: document.querySelector('#ads-view'),
     displayReport: document.querySelector('#report')
 }
 
-// MUDANÇA DE TELAS
+// ============================== FUNÇÕES PARA MUDANÇA DE TELAS ==============================
 const Display = {
     dashboard() { 
         GetDisplay.displayDashboard.style.display = "flex";
         GetDisplay.displayClient.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayClientRegister.style.display = "none";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
     },
     client() { 
         GetDisplay.displayClient.style.display = "flex";
         GetDisplay.displayDashboard.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayClientRegister.style.display = "none";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAdsView.style.display = "none";
     },
-    announcement() { 
-        GetDisplay.displayAnnouncement.style.display = "flex";
+    ads() { 
+        GetDisplay.displayAds.style.display = "flex";
         GetDisplay.displayDashboard.style.display = "none";
         GetDisplay.displayClient.style.display = "none";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayClientRegister.style.display = "none";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAdsView.style.display = "none";
     },
     report() { 
         GetDisplay.displayReport.style.display = "flex";
         GetDisplay.displayDashboard.style.display = "none";
         GetDisplay.displayClient.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
         GetDisplay.displayClientRegister.style.display = "none";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAdsView.style.display = "none";
     },
     clientRegister() {
         GetDisplay.displayClientRegister.style.display = "flex";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayDashboard.style.display = "none";
         GetDisplay.displayClient.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAdsView.style.display = "none";
     },
-    displayAnnouncementRegister() {
-        GetDisplay.displayAnnouncementRegister.style.display = "flex";
+    displayAdsRegister() {
+        GetDisplay.displayAdsRegister.style.display = "flex";
         GetDisplay.displayClientRegister.style.display = "none";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayDashboard.style.display = "none";
         GetDisplay.displayClient.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
-        GetDisplay.displayAnnouncementView.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
+        GetDisplay.displayAdsView.style.display = "none";
     },
-    displayAnnouncementView() {
-        GetDisplay.displayAnnouncementView.style.display = "flex";
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
+    displayAdsView() {
+        GetDisplay.displayAdsView.style.display = "flex";
+        GetDisplay.displayAdsRegister.style.display = "none";
         GetDisplay.displayClientRegister.style.display = "none";
         GetDisplay.displayReport.style.display = "none";
         GetDisplay.displayDashboard.style.display = "none";
         GetDisplay.displayClient.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "none";
+        GetDisplay.displayAds.style.display = "none";
     },
     cancelCreateClient() {
         GetDisplay.displayClientRegister.style.display = "none";
         GetDisplay.displayClient.style.display = "flex";
     },
-    cancelCreateAnnouncement() {
-        GetDisplay.displayAnnouncementRegister.style.display = "none";
-        GetDisplay.displayAnnouncement.style.display = "flex";
+    cancelCreateAds() {
+        GetDisplay.displayAdsRegister.style.display = "none";
+        GetDisplay.displayAds.style.display = "flex";
     }
     
-}
+};
 
-//FORMULÁRIO CADASTRO CLIENTES
 
-const FormClient = {
+// ============================== FUNÇÕES PARA SELEÇÃO DE MENUS ==============================
 
-    //Pegar os elementos do HTML
-    clientName: document.querySelector('input#client-name'),             //Nome do cliente
-    clientPhone: document.querySelector('input#client-phone'),           //Telefone do cliente
-    clientEmail: document.querySelector('input#client-email'),           //E-mail do cliente
-    clientCountry: document.querySelector('input#client-country'),       //País do cliente
+// const SelectedMenu = {
+//     dashboard() {
+//         if(document.querySelector('.dashboard-menu')) {
+//             console.log("Deu certo")
+//         document.querySelector('.dashboard-menu').setAttribute('id','active-menu');
+//         document.querySelector('.client-menu').removeAttribute('id');
+//         document.querySelector('.ads-menu').removeAttribute('id');
+//         document.querySelector('.report-menu').removeAttribute('id');
+//         } else {
+//             console.log("Deu errado")
+//         }
+//     },
+//     client() {
+//         document.querySelector('.client-menu').setAttribute('id','active-menu');
+//         document.querySelector('.dashboard-menu').removeAttribute('id');
+//         document.querySelector('.ads-menu').removeAttribute('id');
+//         document.querySelector('.report-menu').removeAttribute('id');
+//     },
+//     ads() {
+//         document.querySelector('.ads-menu').setAttribute('id','active-menu');
+//         document.querySelector('.client-menu').removeAttribute('id');
+//         document.querySelector('.dashboard-menu').removeAttribute('id');
+//         document.querySelector('.report-menu').removeAttribute('id');
+//     },
+//     report() {
+//         document.querySelector('.report-menu').setAttribute('id','active-menu');
+//         document.querySelector('.client-menu').removeAttribute('id');
+//         document.querySelector('.dashboard-menu').removeAttribute('id');
+//         document.querySelector('.ads-menu').removeAttribute('id');
+//     }
+// };
 
-    //Pegar os valores
-    getValues() {
-        return {
-            clientName: FormClient.clientName.value,               //Nome do cliente
-            clientPhone: FormClient.clientPhone.value,             //Telefone do cliente
-            clientEmail: FormClient.clientEmail.value,             //E-mail do cliente
-            clientCountry: FormClient.clientCountry.value,         //País do cliente
-        }
+
+
+// ============================== LISTA DE CLIENTES CADASTRADOS ==============================
+const FormClient = [  //Array onde será armazenado os dados do input de cadastro de clientes
+    {
+        clientName: "Emerson",
+        clientPhone: 5547999799017,
+        clientEmail: "seiler.emerson@gmail.com",
+        clientCountry: "Brasil"
+    },
+    {
+        clientName: "Mayara",
+        clientPhone: 5547996528676,
+        clientEmail: "mayara@gmail.com",
+        clientCountry: "Estados Unidos"
+    },
+    {
+        clientName: "Evina",
+        clientPhone: 5541996528784,
+        clientEmail: "evina@gmail.com",
+        clientCountry: "Portugal"
+    },
+];
+
+
+// ============================== LISTA DE ANÚNCIOS CADASTRADOS ==============================
+const FormAds = [  //Array onde será armazenado os dados do input de cadastro de anuncios
+    {
+        adsName: "Campanha do Emerson",
+        adsClient: "Emerson Company",
+        adsTotalInvestiment: "50.254.254,00",
+        adsView: "855.548",
+        adsClicks: "125.254",
+        adsShare: "985.124"
+    },
+    {
+        adsName: "Campanha da Mayara",
+        adsClient: "Mayara Company",
+        adsTotalInvestiment: "50.254.254,00",
+        adsView: "855.548",
+        adsClicks: "125.254",
+        adsShare: "985.124"
+    },
+    {
+        adsName: "Campanha da Evina",
+        adsClient: "Evina Company",
+        adsTotalInvestiment: "50.254.254,00",
+        adsView: "855.548",
+        adsClicks: "125.254",
+        adsShare: "985.124"
+    },
+    {
+        adsName: "Campanha do Sheldon",
+        adsClient: "Sheldon Company",
+        adsTotalInvestiment: "50.254.254,00",
+        adsView: "855.548",
+        adsClicks: "125.254",
+        adsShare: "985.124"
+    },
+    
+];
+
+
+// ============================== PEGAR OS DADOS DO MEU FORMULÁRIO DE CLIENTES E COLOCAR NO HTML ==============================
+const dataClient = {
+    dataClientContainer : document.querySelector('#data-client tbody'), //Pega o elemento tbody de dentro do item com id data-client
+
+    addClient(client, index) {  //Responsável por adicionar os dados no htmlClient
+        const trClient = document.createElement('tr');  //Cria um elemento tr na DOM
+        trClient.innerHTML = dataClient.innerHTMLClientTrasaction(client);  //Recebe os dados que vão dentro do td vindos do innerHTMLClientTrasaction
+    
+        dataClient.dataClientContainer.appendChild(trClient)  //Acessa o conteúdo de dataClientContainer e o appendChild lista o elemento trClient criado
+    },
+    innerHTMLClientTrasaction(client) {         //Funcionalidade para fazer a substituição dos itens das linhas do form de clientes pelos clientes existentes no array FormClient
+        //Template dos dados que vão aparecer na tabela
+        const htmlClient = `
+                <td>${client.clientName}</td>
+                <td>${client.clientPhone}</td>
+                <td>${client.clientEmail}</td>
+                <td>${client.clientCountry}</td>
+                <td><h2>x</h2></td>
+        `
+        return htmlClient
+    }
+};
+
+// ============================== EXIBINDO OS CLIENTES EXISTENTES DENTRO DO ARRAY DE CLIENTES FormClient ==============================
+FormClient.forEach(function(FormClient) {  //Para cada item dentro de FormAds
+    dataClient.addClient(FormClient)    //Pegar os dados dos clientes em FormClient e monta dentro da estrutura de tabela de dataClient
+});
+
+// ============================== PEGAR OS DADOS DO MEU FORMULÁRIO DE ANUNCIOS E COLOCAR NA PÁGINA DE ANÚNCIOS ==============================
+
+const dataAds = {
+    dataAdsContainer: document.querySelector('#data-ads tbody'), //Pega o elemento tbody de dentro do item com id data-ads
+    
+    
+
+    addAds(ads, index) {  //Responsável por adicionar os dados no htmlAdd
+        const trAds = document.createElement('tr');  //Cria um elemento tr na DOM
+        trAds.innerHTML = dataAds.innerHTMLAdsTrasaction(ads);  //Recebe os dados que vão dentro do td vindos do innerHTMLAddTrasaction
+    
+        dataAds.dataAdsContainer.appendChild(trAds)  //Acessa o conteúdo de dataAdsContainer e o appendChild lista o elemento trClient criado
+        
+        
     },
 
-    //Validar campos
-    validateFields() {
-        const { clientName, clientPhone, clientEmail, clientCountry} = FormClient.getValues();   //Vai pegar os valores dos campos e separa-los.
+        innerHTMLAdsTrasaction(ads) {         //Funcionalidade para fazer a substituição dos itens das linhas do form de clientes pelos clientes existentes no array FormClient
 
-        if( clientName.trim() === "" || clientPhone.trim() === "" || clientEmail.trim() === "" || clientCountry.trim() === "") {//Verificando se há espaços vazios
-            throw new Error("Por favor, preencha todos os campos")
-        }
+        //Template dos dados que vão aparecer na tabela
+        const htmlAds = `
+            <tr>
+                <td>${ads.adsName}</td>
+                <td>${ads.adsClient}</td>
+                <td>${ads.adsTotalInvestiment}</td>
+                <td>${ads.adsView}</td>
+                <td>${ads.adsClicks}</td>
+                <td>${ads.adsShare}</td>
+                <td><h2 onclick="Display.displayAds()">x</h2></td>
+            </tr>
+        `
+        return htmlAds
     }
-}
+};
+
+// ============================== EXIBINDO OS ANUNCIOS EXISTENTES DENTRO DO ARRAY DE ANUNCIOS FormAds ==============================
+FormAds.forEach(function(FormAds) {  //Para cada item dentro de FormAds
+    dataAds.addAds(FormAds)  //Pegar os dados dos clientes em FormAds e monta dentro da estrutura de tabela de dataAds
+});
+
+
+// ============================== PEGAR OS DADOS DO MEU FORMULÁRIO DE ANUNCIOS E COLOCAR NA PÁGINA DE DASHBOARD ==============================
+const dataAdsDashboard = {
+    dataAdsContainer: document.querySelector('#data-dashboard tbody'), //Pega o elemento tbody de dentro do item com id data-ads
+    
+    
+
+    addAds(ads, index) {  //Responsável por adicionar os dados no htmlAdd
+        const trAds = document.createElement('tr');  //Cria um elemento tr na DOM
+        trAds.innerHTML = dataAdsDashboard.innerHTMLAdsTrasaction(ads);  //Recebe os dados que vão dentro do td vindos do innerHTMLAddTrasaction
+    
+        dataAdsDashboard.dataAdsContainer.appendChild(trAds)  //Acessa o conteúdo de dataAdsContainer e o appendChild lista o elemento trClient criado
+        
+        
+    },
+
+        innerHTMLAdsTrasaction(ads) {         //Funcionalidade para fazer a substituição dos itens das linhas do form de clientes pelos clientes existentes no array FormClient
+
+        //Template dos dados que vão aparecer na tabela
+        const htmlAds = `
+            <tr>
+                <td>${ads.adsName}</td>
+                <td>${ads.adsClient}</td>
+                <td>${ads.adsTotalInvestiment}</td>
+                <td>${ads.adsView}</td>
+                <td>${ads.adsClicks}</td>
+                <td>${ads.adsShare}</td>
+                <td><h2 onclick="Display.displayAds()">x</h2></td>
+            </tr>
+        `
+        return htmlAds
+    }
+};
+
+// ============================== EXIBINDO OS ANUNCIOS EXISTENTES DENTRO DO ARRAY DE ANUNCIOS FormAds ==============================
+FormAds.forEach(function(FormAds) {  //Para cada item dentro de FormAds
+    dataAdsDashboard.addAds(FormAds)  //Pegar os dados dos clientes em FormAds e monta dentro da estrutura de tabela de dataAds
+});
+
+// ============================== PEGAR OS DADOS DO MEU FORMULÁRIO DE ANUNCIOS E COLOCAR NA PÁGINA DE RELATÓRIOS ==============================
+const dataAdsReport = {
+    dataAdsContainer: document.querySelector('#data-report tbody'), //Pega o elemento tbody de dentro do item com id data-ads
+    
+    addAds(ads, index) {  //Responsável por adicionar os dados no htmlAdd
+        const trAds = document.createElement('tr');  //Cria um elemento tr na DOM
+        trAds.innerHTML = dataAdsReport.innerHTMLAdsTrasaction(ads);  //Recebe os dados que vão dentro do td vindos do innerHTMLAddTrasaction
+    
+        dataAdsReport.dataAdsContainer.appendChild(trAds)  //Acessa o conteúdo de dataAdsReport e o appendChild lista o elemento trClient criado
+    },
+
+        innerHTMLAdsTrasaction(ads) {         //Funcionalidade para fazer a substituição dos itens das linhas do form de clientes pelos clientes existentes no array FormClient
+
+        //Template dos dados que vão aparecer na tabela
+        const htmlAds = `
+            <tr>
+                <td>${ads.adsName}</td>
+                <td>${ads.adsClient}</td>
+                <td>${ads.adsTotalInvestiment}</td>
+                <td>${ads.adsView}</td>
+                <td>${ads.adsClicks}</td>
+                <td>${ads.adsShare}</td>
+                <td><h2 onclick="Display.displayAds()">x</h2></td>
+            </tr>
+        `
+        return htmlAds
+    }
+};
+
+// ============================== EXIBINDO OS ANUNCIOS EXISTENTES DENTRO DO ARRAY DE ANUNCIOS FormAds ==============================
+FormAds.forEach(function(FormAds) {  //Para cada item dentro de FormAds
+    dataAdsReport.addAds(FormAds)  //Pegar os dados dos clientes em FormAds e monta dentro da estrutura de tabela de dataAds
+});
+
+
+
+
+
+// ==============================   ==============================
